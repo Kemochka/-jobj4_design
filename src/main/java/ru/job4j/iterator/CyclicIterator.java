481d6245c9b920ae7296c8b6b372612c2ed17715
kemochka <kemaevak@yandex.ru>
2024-01-27 21:06:29 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,33 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+    private List<T> data;
+    private int cursor;
+
+    public CyclicIterator(List<T> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (data.isEmpty()) {
+            return false;
+        }
+        if (cursor == data.size()) {
+            cursor = 0;
+        }
+        return true;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data.get(cursor++);
+    }
+}
