5660e268c7c53e74f1fe50c9e4f89a4ca535822d
kemochka <kemaevak@yandex.ru>
2024-02-19 20:52:20 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines()
+                    .filter(line -> !line.startsWith("#") && line.contains("="))
+                    .forEach(line -> {
+                        String[] parts = line.split("=", 2);
+                        values.put(parts[0], parts[1]);
+                        if (line.startsWith("=")
+                                || parts[0].endsWith("=")
+                                || !line.contains("=")) {
+                            throw new IllegalArgumentException
+                                    ("File contains lines that do not match the key=value pattern");
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner output = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(output::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+        System.out.println(new Config("data/test.properties"));
+        System.out.println(new Config("data/test1.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class ConfigTest {
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
+    }
+
+    @Test
+    void whenFileWithPatternViolation() {
+        String path = "./data/test.properties";
+        Config config = new Config(path);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, config::load);
+        assertThat(exception.getMessage()).isEqualTo("File contains lines that do not match the key=value pattern");
+    }
+
+
+    @Test
+    void whenFileWithSpecialCharactersInValue() {
+        String path = "./data/test1.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("key1")).isEqualTo("value1=1");
+        assertThat(config.value("key2")).isEqualTo("value=");
+    }
+}
