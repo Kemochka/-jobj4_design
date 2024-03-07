de1ccb373b0da680d4e0e4fc8672ee0423850e0a
kemochka <kemaevak@yandex.ru>
2024-03-07 19:08:30 +0300

    2. Формат JSON [#313164 #485062]

--- src/main/java/ru/job4j/serialization/Json.java
+++ src/main/java/ru/job4j/serialization/Json.java
@@ -6,7 +6,7 @@ import com.google.gson.GsonBuilder;
 public class Json {
     public static void main(String[] args) {
         JsonCar jsonCar = new JsonCar(false, 5, "Toyota",
-                new Contact(5,"11-111"), new String[]{"Personal, Family"});
+                new Contact(5, "11-111"), new String[]{"Personal, Family"});
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(jsonCar));
         final String carJson =
