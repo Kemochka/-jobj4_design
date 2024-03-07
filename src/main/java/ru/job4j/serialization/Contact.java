bb9504adbcc4c474426cb89f14d3fc9d4a78704d
kemochka <kemaevak@yandex.ru>
2024-03-07 16:31:26 +0300

    1. Что такое Сериализация? [#313163 #485039]

--- src/main/java/ru/job4j/serialization/Contact.java
+++ src/main/java/ru/job4j/serialization/Contact.java
@@ -2,23 +2,9 @@ package ru.job4j.serialization;
 
 import java.io.*;
 
-public class Contact implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private final int zipCode;
-    private final String phone;
-
-    public Contact(int zipCode, String phone) {
-        this.zipCode = zipCode;
-        this.phone = phone;
-    }
-
-    public int getZipCode() {
-        return zipCode;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
+public record Contact(int zipCode, String phone) implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 340784786541522499L;
 
     @Override
     public String toString() {
@@ -27,6 +13,7 @@ public class Contact implements Serializable {
                 ", phone='" + phone + '\'' +
                 '}';
     }
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         final Contact contact = new Contact(123456, "+7 (111) 111-11-11");
         try (FileOutputStream fos = new FileOutputStream("data/contactSerialization.txt");
