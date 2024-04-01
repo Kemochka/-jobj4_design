1c8a51597f5ab96a95b41fc7fd30b5682ec3ce04
kemochka <kemaevak@yandex.ru>
2024-04-01 18:28:03 +0300

    2. Триггеры [#504803 #489242]

--- db/products_trigger.sql
+++ db/products_trigger.sql
@@ -7,9 +7,7 @@ create table products(
 );
 
 insert into products (id, name, producer, count, price)
-VALUES (1, 'product_3', 'producer_3', 8, 115);
-insert into products (id, name, producer, count, price)
-VALUES (2, 'product_1', 'producer_1', 3, 50);
+VALUES (1, 'product_1', 'producer_2', 8, 115);
 
 create
 or replace function tax()
@@ -24,7 +22,7 @@ $$
 $$
 LANGUAGE 'plpgsql';
 
-create trigger tax_trigger_prod
+create trigger tax_trigger
     after insert
     on products
     referencing new table as
@@ -33,6 +31,8 @@ create trigger tax_trigger_prod
     execute procedure tax();
 
 insert into products (id, name, producer, count, price)
+VALUES (2, 'product_2', 'producer_2', 3, 50);
+insert into products (id, name, producer, count, price)
 VALUES (3, 'product_4', 'producer_4', 8, 115);
 
 create
@@ -40,9 +40,7 @@ or replace function taxProd()
     returns trigger as
 $$
     BEGIN
-        update products
-        set price = price + price * 0.2
-        where id = new.id;
+        new.price = new.price + new.price * 0.2;
         return NEW;
     END;
 $$
@@ -52,10 +50,10 @@ create trigger taxProd_trigger
     before insert
     on products
     for each row
-    execute procedure taxProd();
+    execute function taxProd();
 
 insert into products (id, name, producer, count, price)
-VALUES (5, 'product_6', 'producer_6', 4, 16);
+VALUES (3, 'product_6', 'producer_6', 4, 16);
 
 create
 or replace function history()
@@ -67,7 +65,7 @@ values (new.name, new.price, now());
 return new;
 end;
 $$
-LANGUAGE 'plpgsql';;
+LANGUAGE 'plpgsql';
 
 create trigger history_of_price
     after insert
@@ -76,6 +74,7 @@ create trigger history_of_price
     execute procedure history();
 
 insert into products (id, name, producer, count, price)
-VALUES (6, 'product_7', 'producer_7', 8, 456);
-
+VALUES (4, 'product_7', 'producer_7', 8, 456);
+insert into products (id, name, producer, count, price)
+VALUES (5, 'product_8', 'producer_9', 9, 46);
 
