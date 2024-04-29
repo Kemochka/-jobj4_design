ed035d029cfbcf85552be2a8d12a258f558fb4ea
kemochka <kemaevak@yandex.ru>
2024-04-29 13:34:45 +0300

    2. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/main/java/ru/job4j/kiss/fool/Fool.java
@@ -0,0 +1,46 @@
+package ru.job4j.kiss.fool;
+
+import java.util.Scanner;
+
+public class Fool {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUSS = "FizzBuzz";
+    public static void main(String[] args) {
+        System.out.println("Игра FizzBuzz.");
+        var startAt = 1;
+        var input = new Scanner(System.in);
+        while (startAt < 100) {
+            checkNum(startAt);
+            startAt++;
+            var answer = input.nextLine();
+            startAt = getAnswer(startAt, answer);
+            startAt++;
+        }
+    }
+
+    private static int getAnswer(int startAt, String answer) {
+        String expectedAnswer = calculateAnswer(startAt);
+            if (!expectedAnswer.equals(answer)) {
+                System.out.println("Ошибка. Начинай снова.");
+                return 0;
+        }
+        return startAt;
+    }
+
+    private static void checkNum(int startAt) {
+        System.out.println(calculateAnswer(startAt));
+    }
+
+    private static String calculateAnswer(int num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            return FIZZBUSS;
+        } else if (num % 3 == 0) {
+           return FIZZ;
+        } else if (num % 5 == 0) {
+           return BUZZ;
+        } else {
+            return String.valueOf(num);
+        }
+    }
+}
