55a5345dbf871c27040037b5f5e2dfb8d4a686f4
kemochka <kemaevak@yandex.ru>
2024-05-19 17:20:00 +0300

    2. Парковка машин [#853 #496630]

--- src/main/java/ru/job4j/ood/lsp/parking/Car.java
+++ src/main/java/ru/job4j/ood/lsp/parking/Car.java
@@ -1,14 +1,8 @@
 package ru.job4j.ood.lsp.parking;
 
 public class Car implements Transport {
-    private final int size;
-
-    public Car() {
-        this.size = 1;
-    }
-
     @Override
     public int getSize() {
-        return this.size;
+        return 0;
     }
 }
--- src/main/java/ru/job4j/ood/lsp/parking/Truck.java
+++ src/main/java/ru/job4j/ood/lsp/parking/Truck.java
@@ -1,14 +1,9 @@
 package ru.job4j.ood.lsp.parking;
 
 public class Truck implements Transport {
-    private final int size;
-
-    public Truck(int size) {
-        this.size = size;
-    }
 
     @Override
     public int getSize() {
-        return this.size;
+        return 0;
     }
 }
